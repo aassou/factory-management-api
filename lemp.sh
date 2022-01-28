f5f41defe4f43ca2eee914428a56925bfaaa9f2e
aassou <aassou.abdelilah@gmail.com>
2022-01-28 21:23:41 +0100

    Updated Shell script and added nginx config file

--- lemp.sh
+++ lemp.sh
@@ -15,11 +15,7 @@ sudo add-apt-repository ppa:ondrej/php -y
 
 sudo apt update
 
-sudo apt install php8.0-cli unzip -y
-
-sudo apt install php8.0-fpm -y
-
-sudo apt install php8.0-mysql php8.0-gd php8.0-xml php8.0-mbstring -y
+sudo apt install unzip php8.0-cli php8.0-common php8.0-mysql php8.0-fpm php8.0-gd php8.0-xml php8.0-mbstring -y
 
 sudo apt install nginx -y
 
